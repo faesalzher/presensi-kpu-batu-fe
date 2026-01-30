13566953e6e3f9c85d1088895a07f102252578bc
faesalherlambang <faesalher@gmail.com>
2026-01-30 14:17:29 +0700

    perbaikan error build

--- src/pages/correction/StatusCorrectionPage.tsx
+++ src/pages/correction/StatusCorrectionPage.tsx
@@ -1,5 +1,5 @@
 // src/pages/correction/StatusCorrectionPage.tsx
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Box,
   Typography,
--- src/pages/leave/PersetujuanPage.tsx
+++ src/pages/leave/PersetujuanPage.tsx
@@ -1,5 +1,5 @@
 // src/pages/leave/PersetujuanPage.tsx
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Box,
   Container,
